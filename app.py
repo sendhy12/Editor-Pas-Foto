b834627e3885cee1b6904f0c4d044da68a24df76
Sendhy Maula Ammarulloh <116771792+sendhy12@users.noreply.github.com>
2025-09-24 17:07:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -121,8 +121,16 @@ if uploaded_file:
                 # Konversi grayscale (full hitam putih natural)
                 if grayscale:
                     result = ImageOps.grayscale(result).convert("RGB")
-                    enhancer = ImageEnhance.Contrast(result)
-                    result = enhancer.enhance(1.2)  # 1.0 = normal, >1 = lebih kontras
+                
+                    # Slider untuk atur kecerahan & kontras
+                    brightness_val = st.slider("Kecerahan", 0.5, 2.0, 1.0, 0.1)
+                    contrast_val = st.slider("Kontras", 0.5, 2.0, 1.0, 0.1)
+                
+                    # Terapkan brightness
+                    result = ImageEnhance.Brightness(result).enhance(brightness_val)
+                    # Terapkan contrast
+                    result = ImageEnhance.Contrast(result).enhance(contrast_val)
+
 
                 # Tampilkan hasil
                 st.success("✅ Foto berhasil diproses!")
@@ -149,5 +157,6 @@ if uploaded_file:
 
 else:
     st.info("📤 Unggah foto untuk mulai mengedit.")
+
 
 
